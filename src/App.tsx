11a7f1a1a39695c3cf5ed09baeb6c062dfbcd2cf
sen9712 <986843290@qq.com>
2021-12-23 10:00:09 +0800

    导航条样式完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-/*
- * @Author: your name
- * @Date: 2021-12-09 11:58:46
- * @LastEditTime: 2021-12-21 17:32:04
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \my-app\src\App.tsx
- */
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import styled from "styled-components";
+import Nav from "component/Nav";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -20,17 +13,7 @@ const Main = styled.div`
   overflow: auto;
 `;
 
-const Nav = styled.nav`
-  border: 1px solid blue;
-  > ul {
-    display: flex;
-    > li {
-      width: 33.3333%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
-`;
+
 
 
 function App() {
@@ -52,19 +35,7 @@ function Layout() {
       <Main>
         <Outlet />
       </Main>
-      <Nav>
-        <ul>
-          <li>
-            <Link to="/">标签页</Link>
-          </li>
-          <li>
-            <Link to="/money">记账页</Link>
-          </li>
-          <li>
-            <Link to="/statistics">统计页</Link>
-          </li>
-        </ul>
-      </Nav>
+      <Nav/>
     </Wrapper>
   );
 }
