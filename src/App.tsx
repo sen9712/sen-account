39fece2cddd53efa25453b11c1f560975880845a
sen9712 <986843290@qq.com>
2021-12-29 10:57:24 +0800

    创建Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import styled from "styled-components";
-import Nav from "component/Nav";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from "component/Layout";
+import { Routes, Route, Link } from "react-router-dom";
 
 
 
@@ -19,48 +7,35 @@ const Main = styled.div`
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Tags />} />
+        <Route path="/" element={<Tags />}/>
         <Route path="money" element={<Money />} />
         <Route path="statistics" element={<Statistics />} />
         <Route path="*" element={<NoMatch />} />
-      </Route>
     </Routes>
-  );
-}
-
-function Layout() {
-  return (
-    <Wrapper>
-      <Main>
-        <Outlet />
-      </Main>
-      <Nav/>
-    </Wrapper>
   );
 }
 
 function Tags() {
   return (
-    <div>
-      <h2>标签页</h2>
-    </div>
+    <Layout>
+    <h2>标签页</h2>
+    </Layout>
   );
 }
 
 function Money() {
   return (
-    <div>
-      <h2>记账页</h2>
-    </div>
+    <Layout>
+    <h2>记账页</h2>
+    </Layout>
   );
 }
 
 function Statistics() {
   return (
-    <div>
-      <h2>统计页</h2>
-    </div>
+    <Layout>
+    <h2>统计页</h2>
+    </Layout>
   );
 }
 
---
+++ src/component/Layout.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import Nav from "component/Nav";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Layout = (props: any) => {
+  return (
+    <Wrapper>
+    <Main>
+    {props.children}
+    </Main>
+    <Nav/>
+  </Wrapper>
+  )
+}
+
+export default Layout
