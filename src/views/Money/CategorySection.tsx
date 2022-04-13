b05e32da0ed1003ecdeb12cc3e3ff3ae844b5458
sen9712 <986843290@qq.com>
2022-04-13 11:10:59 +0800

    NumberPadSection数字面板的基本功能完成

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const   CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   > ul{
     display:flex;
@@ -23,5 +24,31 @@ const   CategorySection = styled.section`
     }
   }
 `
+
+const CategorySection: React.FC = () => {
+  const categoryMap = {'-': '支出', '+': '收入'}
+  type Keys = keyof typeof  categoryMap
+  const [categoryList] = useState<Keys[]>(['-', '+'])
+  const [category, setCategory] = useState('-')
+  return (
+    <Wrapper>
+      <ul>
+        {
+          categoryList.map(c => {
+            return (
+              <li
+                key={c}
+                onClick={() => setCategory(c)}
+                className={category === c ? 'selected' : ''}
+              >
+                {categoryMap[c]}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </Wrapper>
+  )
+}
 
 export {CategorySection}
